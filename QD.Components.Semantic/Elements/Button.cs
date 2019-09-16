7ee6ffe56896b923afb2f624f0c7be8de420c231
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2019-09-17 00:18:00 +0200

    Refactor and documentation

--- QD.Components.Semantic/Elements/Button.cs
+++ QD.Components.Semantic/Elements/Button.cs
@@ -1,34 +1,47 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using QD.Components.Semantic.Enums;
+using QD.Components.Semantic.Enums.Button;
 
 namespace QD.Components.Semantic.Elements
 {
+	/// <summary>
+	/// A Button indicates a possible user action.
+	/// </summary>
 	public class Button : ComponentBase
 	{
 		/// <summary>
-		/// Primary Button
+		/// A button can be formatted to show different levels of emphasis.
 		/// </summary>
 		[Parameter]
 		public bool Primary { get; set; }
 
+		/// <summary>
+		/// A button can be formatted to show different levels of emphasis.
+		/// </summary>
 		[Parameter]
 		public bool Secondary { get; set; }
 
+		/// <summary>
+		/// A button can animate to show hidden content.
+		/// </summary>
 		[Parameter]
-		public ButtonAnimation Animated { get; set; }
+		public Animation Animated { get; set; }
 
+		/// <summary>
+		/// Called after user's click.
+		/// </summary>
 		[Parameter]
 		public EventCallback<MouseEventArgs> OnClick { get; set; }
 
+		/// <inheritdoc />
 		protected override void ConfigureComponent()
 		{
 			ElementTag = "button";
 			ElementClass = "ui";
 
-			if (Animated != ButtonAnimation.None)
+			if (Animated != Animation.None)
 			{
-				if (Animated != ButtonAnimation.Horizontal)
+				if (Animated != Animation.Horizontal)
 				{
 					ElementClass = $"{ElementClass} {Animated.ToString().ToLowerInvariant()}";
 				}
--- QD.Components.Semantic/Enums/Color.cs
+++ QD.Components.Semantic/Enums/Color.cs
@@ -7,32 +7,74 @@ namespace QD.Components.Semantic.Enums
 	/// </summary>
 	public enum Color
 	{
+		/// <summary>
+		/// None
+		/// </summary>
 		[Description("none")]
 		None,
+		/// <summary>
+		/// Red color
+		/// </summary>
 		[Description("red")]
 		Red,
+		/// <summary>
+		/// Orange color
+		/// </summary>
 		[Description("orange")]
 		Orange,
+		/// <summary>
+		/// Yellow color
+		/// </summary>
 		[Description("yellow")]
 		Yellow,
+		/// <summary>
+		/// Olive color
+		/// </summary>
 		[Description("olive")]
 		Olive,
+		/// <summary>
+		/// Green color
+		/// </summary>
 		[Description("green")]
 		Green,
+		/// <summary>
+		/// Teal color
+		/// </summary>
 		[Description("teal")]
 		Teal,
+		/// <summary>
+		/// Blue color
+		/// </summary>
 		[Description("blue")]
 		Blue,
+		/// <summary>
+		/// Violet color
+		/// </summary>
 		[Description("violet")]
 		Violet,
+		/// <summary>
+		/// Purple color
+		/// </summary>
 		[Description("purple")]
 		Purple,
+		/// <summary>
+		/// Pink color
+		/// </summary>
 		[Description("pink")]
 		Pink,
+		/// <summary>
+		/// Brown color
+		/// </summary>
 		[Description("brown")]
 		Brown,
+		/// <summary>
+		/// Grey color
+		/// </summary>
 		[Description("grey")]
 		Grey,
+		/// <summary>
+		/// Black color
+		/// </summary>
 		[Description("black")]
 		Black
 	}
