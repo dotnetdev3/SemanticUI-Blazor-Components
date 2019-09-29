aef2a226a7258b354f6c6d1f35a50d304695d05c
microsoftv3 <stronghuman0207@gmail.com>
2019-09-29 23:24:37 +0200

    Added parameters from label component

---
+++ QD.Components.Semantic/Enums/Label/Corner.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace QD.Components.Semantic.Enums.Label
+{
+	/// <summary>
+	/// Corner
+	/// </summary>
+	public enum Corner
+	{
+		/// <summary>
+		/// None
+		/// </summary>
+		[Description("none")]
+		None,
+		/// <summary>
+		/// Left corner
+		/// </summary>
+		[Description("left")]
+		Left,
+		/// <summary>
+		/// Right corner
+		/// </summary>
+		[Description("right")]
+		Right,
+	}
+}
---
+++ QD.Components.Semantic/Enums/Label/Ribbon.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace QD.Components.Semantic.Enums.Label
+{
+	/// <summary>
+	/// Ribbon
+	/// </summary>
+	public enum Ribbon
+	{
+		/// <summary>
+		/// None
+		/// </summary>
+		[Description("none")]
+		None,
+		/// <summary>
+		/// Left ribbon
+		/// </summary>
+		[Description("left")]
+		Left,
+		/// <summary>
+		/// Right ribbon
+		/// </summary>
+		[Description("right")]
+		Right,
+	}
+}
