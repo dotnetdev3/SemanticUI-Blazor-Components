46b9d552531e070623dd862b8265acb5783b66a9
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2019-09-29 23:24:37 +0200

    Added parameters from label component

---
+++ QD.Components.Semantic/Enums/Label/Side.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+
+namespace QD.Components.Semantic.Enums.Label
+{
+	/// <summary>
+	/// Side
+	/// </summary>
+	public enum Side
+	{
+		/// <summary>
+		/// None
+		/// </summary>
+		[Description("none")]
+		None,
+		/// <summary>
+		/// Top side
+		/// </summary>
+		[Description("top")]
+		Top,
+		/// <summary>
+		/// Bottom side
+		/// </summary>
+		[Description("bottom")]
+		Bottom,
+		/// <summary>
+		/// Top left side
+		/// </summary>
+		[Description("top left")]
+		TopLeft,
+		/// <summary>
+		/// Top right side
+		/// </summary>
+		[Description("top right")]
+		TopRight,
+		/// <summary>
+		/// Bottom left side
+		/// </summary>
+		[Description("bottom left")]
+		BottomLeft,
+		/// <summary>
+		/// Bottom right side
+		/// </summary>
+		[Description("bottom right")]
+		BottomRight
+	}
+}
