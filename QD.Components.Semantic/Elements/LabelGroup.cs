7a455b0d651916df31839f22e9f3308227888e43
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2019-09-30 22:11:33 +0200

    Added Label Group

---
+++ QD.Components.Semantic/Elements/LabelGroup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Components;
+using QD.Components.Semantic.Enums;
+
+namespace QD.Components.Semantic.Elements
+{
+	/// <summary>
+	/// A label can be grouped.
+	/// </summary>
+	public class LabelGroup : ComponentBase
+	{
+		/// <summary>
+		/// Labels can share shapes.
+		/// </summary>
+		[Parameter]
+		public bool Circular { get; set; }
+
+		/// <summary>
+		/// Label group can share colors together.
+		/// </summary>
+		[Parameter]
+		public Color Color { get; set; }
+
+		/// <summary>
+		/// Label group can share sizes together.
+		/// </summary>
+		[Parameter]
+		public Size Size { get; set; }
+
+		/// <summary>
+		/// Label group can share tag formatting.
+		/// </summary>
+		[Parameter]
+		public bool Tag { get; set; }
+
+		/// <inheritdoc />
+		protected override void ConfigureComponent()
+		{
+			ElementClass = "ui";
+
+			if (Size != Size.None)
+			{
+				ElementClass = $"{ElementClass} {Size.GetDescription()}";
+			}
+
+			if (Color != Color.None)
+			{
+				ElementClass = $"{ElementClass} {Color.GetDescription()}";
+			}
+
+			if (Tag)
+			{
+				ElementClass = $"{ElementClass} tag";
+			}
+
+			if (Circular)
+			{
+				ElementClass = $"{ElementClass} circular";
+			}
+
+			ElementClass = $"{ElementClass} labels";
+		}
+	}
+}
