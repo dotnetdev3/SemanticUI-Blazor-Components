4f3a49234c9854f89bedbff8d450146469a279e3
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2019-10-09 23:02:32 +0200

    Added button parameters

---
+++ QD.Components.Semantic/Enums/Button/Position.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace QD.Components.Semantic.Enums.Button
+{
+	/// <summary>
+	/// Position
+	/// </summary>
+	public enum Position
+	{
+		/// <summary>
+		/// None
+		/// </summary>
+		[Description("none")]
+		None,
+		/// <summary>
+		/// Left position
+		/// </summary>
+		[Description("left")]
+		Left,
+		/// <summary>
+		/// Right position
+		/// </summary>
+		[Description("right")]
+		Right
+	}
+}
