d9a02437a37e6cdc00691e9f80051b70746b1aa3
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2019-10-16 00:55:46 +0200

    ComponentBase Refactor and Button Group - WIP

---
+++ QD.Components.Semantic/Enums/HorizontalPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace QD.Components.Semantic.Enums
+{
+	/// <summary>
+	/// Horizontal position
+	/// </summary>
+	public enum HorizontalPosition
+	{
+		/// <summary>
+		/// None
+		/// </summary>
+		[Description("none")]
+		None,
+		/// <summary>
+		/// Left position
+		/// </summary>
+		[Description("left")]
+		Left,
+		/// <summary>
+		/// Right position
+		/// </summary>
+		[Description("right")]
+		Right,
+	}
+}
---
+++ QD.Components.Semantic/Enums/Position.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace QD.Components.Semantic.Enums
+{
+	/// <summary>
+	/// Position
+	/// </summary>
+	public enum Position
+	{
+		/// <summary>
+		/// None
+		/// </summary>
+		[Description("none")]
+		None,
+		/// <summary>
+		/// Left position
+		/// </summary>
+		[Description("left")]
+		Left,
+		/// <summary>
+		/// Right position
+		/// </summary>
+		[Description("right")]
+		Right,
+		/// <summary>
+		/// Top Position
+		/// </summary>
+		[Description("top")]
+		Top,
+		/// <summary>
+		/// Bottom position
+		/// </summary>
+		[Description("bottom")]
+		Bottom,
+	}
+}
